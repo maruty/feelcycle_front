b78331a5f72365402f0072dbfeab5f986fb67479
maruty <01008973@CA0315.local>
2015-10-02 20:28:06 +0900

    post受け取り系確認

--- regist_controller.php
+++ regist_controller.php
@@ -1,22 +1,33 @@
 <?php
 
+	$FINAL_URL_STRING = "http://52.69.227.6/feelApi?";
+
     //POSTの受け取り
     var_dump($_POST);
     echo "<br />";
     echo "出力確認".$_POST['loginId'];
 
 
-
-
-
     //URL組み立て用
     $url = "";
 
 
+    //共通部
+    $url = $FINAL_URL_STRING."loginId=".$_POST['loginId']."&"
+    						."loginPass=".$_POST['loginPass']."&"
+    						."nickName=".$_POST['nickName']."&"
+    						."feelcycleLoginId1".$_POST['feelcycleLoginId1']."&"
+    						."feelcycleLoginPass1".$_POST['feelcycleLoginPass1']."&"
+    						."feelcycleLoginId2".$_POST['feelcycleLoginId2']."&"
+    						."feelcycleLoginPass2".$_POST['feelcycleLoginPass2']."&"
+    						."remember".$_POST['remember'];
+
+    echo "<br />";
+    echo "<br />";
+    echo "URL出力確認：".$url;
+
 
     //curl初期化
-
-    /*
 
     $conn = curl_init();
 
@@ -24,12 +35,14 @@
 	curl_setopt($conn, CURLOPT_FOLLOWLOCATION, 1);
 	curl_setopt($conn, CURLOPT_RETURNTRANSFER, 1);
 	curl_setopt($conn, CURLOPT_HEADER, true);
-
-	$url = 'http://www.google.co.jp/';
  
 	curl_setopt($conn, CURLOPT_URL, $url);
 	$response = curl_exec($conn);
-	*/
+
+    echo "<br />";
+    echo "<br />";
+	echo "レスポンス確認";
+
 	//curl
 	//curl_close($conn);
 
