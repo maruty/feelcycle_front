2490bde87c0a5309f400bda3bc9d7e2d17ff137f
maruty <01008973@CA0315.local>
2015-10-03 13:01:05 +0900

    ログインフォーム設置

---
+++ mypage_controller.php
@@ -0,0 +1,31 @@
+<?php
+
+    $FINAL_URL_STRING = "http://52.69.227.6/feelcyclebatch/apiRegist/checkUser/?";
+    //URL組み立て用
+    $url = "";
+
+    $url = $FINAL_URL_STRING."loginId=".$_POST['loginId']."&"."loginPass=".$_POST['loginPass'];
+
+    //curl初期化
+    $conn = curl_init();
+
+    curl_setopt($conn, CURLOPT_CONNECTTIMEOUT, 2);
+    curl_setopt($conn, CURLOPT_FOLLOWLOCATION, 1);
+    curl_setopt($conn, CURLOPT_RETURNTRANSFER, 1);
+    curl_setopt($conn, CURLOPT_HEADER, true);
+ 
+    curl_setopt($conn, CURLOPT_URL, $url);
+    $response = curl_exec($conn);
+
+
+
+
+    //ログイン情報の会員が存在するかの確認
+
+
+
+
+
+
+?>
+
