a34069a0e6fa78f95bc350e9236254eff9cf630b
maruty <01008973@CA0315.local>
2015-10-04 10:42:57 +0900

    レッスンをjsonで返す処理作成

--- mypage.php
+++ mypage.php
@@ -42,7 +42,7 @@ $coutOffset = strpos($response, "lesson");
 //echo "<br />";
 $output = substr($response, ($coutOffset-20), strlen($response));
 
-echo $output;
+//echo $output;
 
 // 文字化けするかもしれないのでUTF-8に変換
 $temp = mb_convert_encoding($output, 'UTF8', 'ASCII,JIS,UTF-8,EUC-JP,SJIS-WIN');
