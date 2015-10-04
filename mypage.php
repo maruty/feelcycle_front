0b8f1137befd7255f24e269c61757df5435d0408
maruty <01008973@CA0315.local>
2015-10-04 09:42:13 +0900

    レッスンをjsonで返す処理作成

--- mypage.php
+++ mypage.php
@@ -10,6 +10,28 @@ if(isset($_COOKIE["loginStatus"]) && $_COOKIE["loginStatus"] != ""
     $_SESSION["loginPass"] = $_COOKIE["loginPass"];
 }
 
+$FINAL_URL_STRING = "http://52.69.227.6/feelcyclebatch/apiRegist/getLesson?";
+    //URL組み立て用
+    $url = "";
+
+    $url = $FINAL_URL_STRING."loginId=".$_POST['loginId']."&"."loginPass=".$_POST['loginPass'];
+
+    //curl初期化
+    $conn = curl_init();
+
+    curl_setopt($conn, CURLOPT_CONNECTTIMEOUT, 2);
+    curl_setopt($conn, CURLOPT_FOLLOWLOCATION, 1);
+    curl_setopt($conn, CURLOPT_RETURNTRANSFER, 1);
+    curl_setopt($conn, CURLOPT_HEADER, true);
+ 
+    curl_setopt($conn, CURLOPT_URL, $url);
+    $response = curl_exec($conn);
+
+    echo $response;
+
+
+
+
 //echo "ログインステータス".$_SESSION["loginStatus"];
 ?>
 
