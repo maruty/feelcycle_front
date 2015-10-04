0d70c8ef2a4590c2395de93d7f1b5615e30d4b95
maruty <01008973@CA0315.local>
2015-10-04 17:18:30 +0900

    パース内容を修正

--- mypage.php
+++ mypage.php
@@ -87,7 +87,7 @@ $response = curl_exec($conn);
 $response = str_replace(array("\r\n", "\r", "\n"), '', $response );
 $response = preg_replace('/(\s|　)/','',$response);
 mb_language('Japanese');
-//$response = mb_convert_encoding($response, "UTF-8", "EUC-JP")
+$response = mb_convert_encoding($response, "UTF-8", "EUC-JP")
 
 echo $response;
 
