7a12898fa4bee24a3f1814d2b7cc01d663e6d38f
maruty <01008973@CA0315.local>
2015-10-04 11:06:06 +0900

    レッスンをjsonで返す処理作成

--- mypage.php
+++ mypage.php
@@ -49,18 +49,11 @@ echo $output;
  
 // オブジェクト毎にパース
 // trueを付けると連想配列として分解して格納してくれます。
-$obj = json_decode($output, true);
+//$obj = json_decode($output, true);
 
-// パースに失敗した時は処理終了
-if ($obj === NULL) {
-echo "パース失敗";
-}
-
+$obj = json_decode( $output , false ) ;
 
 var_dump($obj);
-
-
-
 
 //echo "ログインステータス".$_SESSION["loginStatus"];
 ?>
