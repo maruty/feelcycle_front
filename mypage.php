6f46543612f60309444de8280d091b0a42fe0f02
maruty <01008973@CA0315.local>
2015-11-08 15:39:59 +0900

    修正

--- mypage.php
+++ mypage.php
@@ -586,8 +586,10 @@ $shukeiObject = json_decode( $responseShukeiData ,true);
                             alert("通信成功");
                             data = data.replace(/&#034;/g,'"');
                             alert(data);
-                            var json_obj = {};
-                            json_obj = $.parseJSON(data);
+                            //var json_obj = {};
+                            // = $.parseJSON(data);
+
+                            var json_obj = JSON.parse(data);
                             alert(json_obj);
 
                               for(var n in json_obj) {
