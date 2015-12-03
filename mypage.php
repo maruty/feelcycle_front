4f448ff31318f4caee22632b0933f0e714f043bc
maruty <01008973@CA0315.local>
2015-12-03 18:24:26 +0900

    json

--- mypage.php
+++ mypage.php
@@ -580,7 +580,7 @@ $shukeiObject = json_decode( $responseShukeiData ,true);
                                 loginId: 'yanagisawa.trade@gmail.com',
                                 //loginPass: 'yutaka467',
                             },
-                            dataType: 'json',
+                            dataType: 'html',
                             cache: false
                            // contentType: 'application/json'
                         })
