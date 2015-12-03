24e5987e3ac86ad9cd3e479f177174484fe2294c
maruty <01008973@CA0315.local>
2015-12-03 18:37:15 +0900

    json

--- mypage.php
+++ mypage.php
@@ -601,7 +601,7 @@ $shukeiObject = json_decode( $responseShukeiData ,true);
 
                             //var json_obj = JSON.stringify(jsonString);
                             var json_obj;
-                            json_obj = JSON.parse(jsonString);
+                            json_obj = JSON.stringify(jsonString);
 
                             alert("変換後" + json_obj);
                             /*
