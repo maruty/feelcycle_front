0c0ba5a5e2e96b8346b3032df96835fa1d2f4be0
maruty <01008973@CA0315.local>
2015-12-05 15:33:36 +0900

    ajax

--- mypage.php
+++ mypage.php
@@ -601,7 +601,8 @@ $shukeiObject = json_decode( $responseShukeiData ,true);
 
                             //var json_obj = JSON.stringify(jsonString);
                             var json_obj;
-                            json_obj = JSON.stringify(jsonString);
+                            //json_obj = JSON.stringify(jsonString);
+                            json_obj = $.parseJSON(data);
 
                             alert("変換後" + json_obj);
                             /*
