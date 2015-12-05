584b4f45231f56c76258ac1ae25dd5697680b60b
maruty <01008973@CA0315.local>
2015-12-06 02:13:16 +0900

    ajax機能

--- mypage.php
+++ mypage.php
@@ -605,10 +605,10 @@ $shukeiObject = json_decode( $responseShukeiData ,true);
                             $(".lessonData").remove();
 
                             for (var i = 0; i < json_obj.shukei.length; i++) {
-                                    $(".lessonName").before("<tr>");
+                                    $(".lessonName").prepend("<tr>");
                                     $(".rowDisplayedData").append("<td class='lessonName'>" + json_obj.shukei[i].shukeiName + "</td>");
                                     $(".rowDisplayedData").append("<td class='zyukobi'>" + json_obj.shukei[i].shukeiValue+ "</td>");
-                                    $(".zyukobi").after("</tr>");
+                                    $(".zyukobi").append("</tr>");
     
 
                                 
