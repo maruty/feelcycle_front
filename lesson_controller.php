01972773874a6993efc6319e9ded62c60cbf28d1
柳澤　雄高 <yanagisawa_yutaka@r.recruit.co.jp>
2016-09-19 15:22:13 +0900

    自動取得ツール

--- lesson_controller.php
+++ lesson_controller.php
@@ -14,10 +14,25 @@
     // 連想配列($array)をJSONに変換(エンコード)する
     $json = json_encode( $array , JSON_PRETTY_PRINT ) ;
 
+    echo $json;
+
     $filepath = "../json/lesson.json"; // ファイルへのパスを変数に格納
     
     $fp = fopen($filepath, "w"); // 新規書き込みモードで開く
-    @fwrite( $fp, $json, strlen($json) ); // ファイルへの書き込み
+
+    if ($fp){
+        if (flock($fp, LOCK_EX)){
+            if (fwrite( $fp, $json, strlen($json)  === FALSE){
+                print('ファイル書き込みに失敗しました');
+            }else{
+                print($json.'をファイルに書き込みました');
+            }
+
+            flock($fp, LOCK_UN);
+        }else{
+            print('ファイルロックに失敗しました');
+    }
+
     fclose($fp);
     //ファイルへの書き込みは終了
 
