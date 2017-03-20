8f33a93049ca8593c88793a4f1856ebc5fa5f6e0
柳澤　雄高 <yanagisawa_yutaka@r.recruit.co.jp>
2017-03-20 15:05:21 +0900

    b-monster用改修

--- lesson.php
+++ lesson.php
@@ -130,6 +130,22 @@
         </div>
     </div>
 
+
+
+    <!-- jQuery -->
+    <script src="./bower_components/jquery/dist/jquery.min.js"></script>
+
+    <!-- Bootstrap Core JavaScript -->
+    <script src="./bower_components/bootstrap/dist/js/bootstrap.min.js"></script>
+
+    <!-- Metis Menu Plugin JavaScript -->
+    <script src="./bower_components/metisMenu/dist/metisMenu.min.js"></script>
+
+    <!-- Custom Theme JavaScript -->
+    <script src="./dist/js/sb-admin-2.js"></script>
+
+    <!-- bmonster改修-->
+
     <script>
       $(function() {
 
@@ -156,17 +172,6 @@
       });
     </script>
 
-    <!-- jQuery -->
-    <script src="./bower_components/jquery/dist/jquery.min.js"></script>
-
-    <!-- Bootstrap Core JavaScript -->
-    <script src="./bower_components/bootstrap/dist/js/bootstrap.min.js"></script>
-
-    <!-- Metis Menu Plugin JavaScript -->
-    <script src="./bower_components/metisMenu/dist/metisMenu.min.js"></script>
-
-    <!-- Custom Theme JavaScript -->
-    <script src="./dist/js/sb-admin-2.js"></script>
 
 </body>
 
