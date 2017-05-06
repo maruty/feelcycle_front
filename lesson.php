9891865f946372b385b3256438daaab21a5e0034
柳澤　雄高 <yanagisawa_yutaka@r.recruit.co.jp>
2017-05-06 20:09:23 +0900

    カレンダーUI追加

--- lesson.php
+++ lesson.php
@@ -49,6 +49,8 @@
         <script src="https://oss.maxcdn.com/libs/respond.js/1.4.2/respond.min.js"></script>
     <![endif]-->
 
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.2.1/jquery.min.js"></script>
+
     <script>
       $("#datapicker").flatpickr(optional_config);
     </script>
